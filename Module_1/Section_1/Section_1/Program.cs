a4f3ad2e6f6d4d7197997a6b4cf49ff379c700f0
Gabriel2002Can <gabriel2002ca@gmail.com>
2025-01-09 12:03:55 -0400

    First Section Done

--- Module_1/Section_1/Section_1/Program.cs
+++ Module_1/Section_1/Section_1/Program.cs
@@ -10,7 +10,28 @@ namespace Section_1
     {
         static void Main(string[] args)
         {
+            //Starting three question challenge
+            Console.WriteLine("This is the company x question survey, please answer the following questions:\n");
+
+            Console.WriteLine("1 - What is your name?");
+            var NameCustomer = Console.ReadLine();
+
+            Console.WriteLine("\n2 - How old are you?");
+            var AgeCustomer = Console.ReadLine();
+
+            Console.WriteLine("\n3 - What month were you born in?");
+            var MonthCustomer = Console.ReadLine();
+
+            Console.WriteLine("\n4 - Where are you from?");
+            var PlaceCustomer = Console.ReadLine();
+
+            Console.WriteLine("\n5 - What is your favorite movie?");
+            var MovieCustomer = Console.ReadLine();
+
+            Console.WriteLine($"\n{NameCustomer} was born in {PlaceCustomer}, {MonthCustomer} of {AgeCustomer}. Their favorite movie is {MovieCustomer}");
             
+            //Avoiding the program to close after the input part is finished
+            Console.ReadKey();
         }
     }
 }

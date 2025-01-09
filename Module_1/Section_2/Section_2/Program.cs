27d45aebb60bcbbd4ea9d4a88c655da4e54e47b0
Gabriel2002Can <gabriel2002ca@gmail.com>
2025-01-09 15:06:14 -0400

    Section 2 done

---
+++ Module_1/Section_2/Section_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Starting Section 2
+
+            Console.WriteLine("Type your password to enter the system.\nPassword: ");
+
+            var UserGuess = Console.ReadLine();
+
+            if(UserGuess != "secret")
+            {
+                Console.WriteLine("\nThe password is wrong. Please try to log again.");
+                return;
+            }
+
+            Console.WriteLine("\nYour User is authenticated.");
+        }
+    }
+}

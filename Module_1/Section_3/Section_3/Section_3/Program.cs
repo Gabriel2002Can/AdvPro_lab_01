2e7b1cc8ad8a893bf5eca45e42e9fa6383f80ff2
Gabriel2002Can <gabriel2002ca@gmail.com>
2025-01-10 14:46:41 -0400

    section 3 done

---
+++ Module_1/Section_3/Section_3/Section_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Section_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var count = 0; count < 5; count ++)
+            {
+
+                //Restarting after each loop 
+                var i = 1;
+                var operation = +1;
+                
+
+                while (i > 0)
+                {
+                    Console.WriteLine(i);
+                    if (i == 10)
+                    {
+
+                        Console.WriteLine(i);
+
+                        //Inverting operation
+                        operation = -1;
+                    }
+                    i += operation;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}

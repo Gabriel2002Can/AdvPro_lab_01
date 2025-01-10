fa285fa3d67578b26fa31a3e9243e42cdadf849a
Gabriel2002Can <gabriel2002ca@gmail.com>
2025-01-10 15:48:16 -0400

    Section_4 done

--- Module_1/Section_4/Section_4/Section_4/Program.cs
+++ Module_1/Section_4/Section_4/Section_4/Program.cs
@@ -10,6 +10,40 @@ namespace Section_4
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("How many students are you going to register?");
+
+            var studentNumber = int.Parse(Console.ReadLine());
+
+            var Students = new string[studentNumber, 2];
+
+            Console.WriteLine("\n");
+
+            for (int s = 0; s < studentNumber; s++)
+            {
+
+                Console.WriteLine("Input the Student's name:");
+
+                //Name input 
+                Students[s, 0] = Console.ReadLine();
+
+                Console.WriteLine($"Input {Students[s,0]}'s grade");
+
+                //Grade input
+                Students[s,1] = Console.ReadLine();
+
+                Console.WriteLine("\n");
+            }
+
+            Console.WriteLine("\n");
+
+            for (int s = 0; s < studentNumber; s++)
+            {
+                Console.WriteLine(Students[s,0]);
+                Console.WriteLine(Students[s, 1]);
+
+                Console.WriteLine("\n");
+            }
         }
     }
 }

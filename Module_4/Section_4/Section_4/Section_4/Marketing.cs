4c1b14409ac60f2d513d787e36151008d423c7fa
Gabriel2002Can <gabriel2002ca@gmail.com>
2025-01-19 12:10:28 -0400

    section 4 completed

---
+++ Module_4/Section_4/Section_4/Section_4/Marketing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section_4
+{
+    internal class Marketing
+    {
+
+        public void OnPosted()
+        {
+            Console.WriteLine("Thank you for completing the survey. You are now subscribed to ten of our newsletters.");
+        }
+    }
+}
---
+++ Module_4/Section_4/Section_4/Section_4/Stats.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section_4
+{
+    internal class Stats
+    {
+        public void Start()
+        {
+            Program.Posted += HasPosted;
+        }
+
+        void HasPosted()
+        {
+            Console.WriteLine("Survey posted, run stats");
+        }
+    }
+}
